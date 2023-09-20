31eda22937ea64981f930d1bde738c51af62ca8c
rayzalzero <rayzalzero@gmail.com>
2023-09-20 10:51:18 +0700

    refactor(init): change router and db connection

--- internal/app/handler/todo/init.go
+++ internal/app/handler/todo/init.go
@@ -5,7 +5,6 @@ import (
 	domain "github.com/sukha-id/bee/internal/domain/todo"
 	"github.com/sukha-id/bee/pkg/logrusx"
 	"net/http"
-	"time"
 )
 
 type Handler struct {
@@ -21,7 +20,6 @@ func NewHandlerTodo(router *gin.Engine, todoUseCase domain.TodoUseCase, logger *
 	v1 := router.Group("/v1")
 	{
 		v1.GET("ping", func(context *gin.Context) {
-			time.Sleep(6 * time.Second)
 			context.JSON(http.StatusOK, gin.H{
 				"message": "pong",
 			})
