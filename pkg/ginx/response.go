db30a119311c8320743452fddc797bc4f682f108
rayzalzero <rayzalzero@gmail.com>
2023-09-22 08:40:19 +0700

    refactor(init): refactor response

---
+++ pkg/ginx/response.go
@@ -0,0 +1,35 @@
+package ginx
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type generalResponse struct {
+	RequestID string      `json:"request_id,omitempty"`
+	Message   string      `json:"message,omitempty"`
+	Status    int         `json:"status,omitempty"`
+	Error     interface{} `json:"error,omitempty"`
+	Data      interface{} `json:"data,omitempty"`
+}
+
+func RespondWithError(ctx *gin.Context, status int, message string, error interface{}) {
+	ctx.JSON(status, generalResponse{
+		RequestID: ctx.Value("request_id").(string),
+		Message:   message,
+		Status:    status,
+		Error:     error,
+	})
+	ctx.Abort()
+	return
+}
+
+func RespondWithJSON(ctx *gin.Context, status int, message string, data interface{}) {
+	ctx.JSON(status, generalResponse{
+		RequestID: ctx.Value("request_id").(string),
+		Message:   message,
+		Status:    status,
+		Data:      data,
+	})
+	ctx.Abort()
+	return
+}
